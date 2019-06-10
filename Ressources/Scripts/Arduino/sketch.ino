c7942af84c059e5b2086424c4f065db05fd8dd1e
Antoine THYS <contact@antoinethys.com>
2019-06-10 23:35:07 +0200

    Ajout diapo

--- Ressources/Scripts/Arduino/sketch.ino
+++ Ressources/Scripts/Arduino/sketch.ino
@@ -1,7 +1,7 @@
 // Code pour récupérer les valeurs des capteurs
 
-int serial_hum, serial_temp, serial_lum, volt_hum, volt_temp, luminosite, temperature, humidite;
-float Vout, RLDR;
+int luminosite, temperature, humidite;
+float serial_hum, serial_temp, serial_lum, volt_hum, volt_temp,  Vout, RLDR;
 
 // Fonction setup(), appelée au démarrage de la carte Arduino
 void setup()
@@ -24,13 +24,8 @@ void loop()
   // Mesure la tension sur la broche A4 Luminosité
   serial_lum = analogRead(A4);
 
-  // Humidité
-  volt_hum = serial_hum * 5.0;
-  volt_hum /= 1023.0;
-
   // Voltage Température
-  volt_temp = serial_temp * 5.0;
-  volt_temp /= 1023.0;
+  volt_temp = (serial_temp * 5.0) / 1024;
 
   // Calcul luminosité
   Vout = (serial_lum * 0.0048828125);   // [Vout = ADC * (Vin / 1023)]
@@ -39,14 +34,16 @@ void loop()
   //Valeurs cohérentes
 
   // Humidité
-  humidite = (100 - (volt_hum * 100));
+  humidite = serial_hum;
+  
   // Température
   temperature = (volt_temp - 0.5) * 100;
+
   // Luminosité
   luminosite = (500 / RLDR);
 
   // Envoi les mesure à la raspberry pour affichage et attends 250ms
-  Serial.print(humidite);
+  Serial.print(serial_hum);
   Serial.print("\t");
   Serial.print(temperature);
   Serial.print("\t");
