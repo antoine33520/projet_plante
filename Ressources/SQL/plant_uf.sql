64a3bcd8088e11b668a6b697c957d3c876714fda
emmadrd912 <43409946+emmadrd912@users.noreply.github.com>
2019-06-11 09:57:36 +0200

    Add files via upload

--- Ressources/SQL/plant_uf.sql
+++ Ressources/SQL/plant_uf.sql
@@ -2,10 +2,10 @@
 -- version 4.6.6deb4
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Lun 10 Juin 2019 à 17:30
--- Version du serveur :  10.1.38-MariaDB-0+deb9u1
--- Version de PHP :  7.0.33-0+deb9u3
+-- Host: localhost:3306
+-- Generation Time: Jun 11, 2019 at 09:52 AM
+-- Server version: 10.1.38-MariaDB-0+deb9u1
+-- PHP Version: 7.0.33-0+deb9u3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,18 +17,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `plant_uf`
+-- Database: `plant_uf`
 --
-CREATE DATABASE IF NOT EXISTS `plant_uf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `plant_uf`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `datas`
+-- Table structure for table `datas`
 --
 
-DROP TABLE IF EXISTS `datas`;
 CREATE TABLE `datas` (
   `data_id` int(11) NOT NULL,
   `data_luminosity` float NOT NULL,
@@ -39,7 +36,7 @@ CREATE TABLE `datas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `datas`
+-- Dumping data for table `datas`
 --
 
 INSERT INTO `datas` (`data_id`, `data_luminosity`, `data_humidity`, `data_temperature`, `data_timestamp`, `uplant_id`) VALUES
@@ -49,10 +46,9 @@ INSERT INTO `datas` (`data_id`, `data_luminosity`, `data_humidity`, `data_temper
 -- --------------------------------------------------------
 
 --
--- Structure de la table `plants`
+-- Table structure for table `plants`
 --
 
-DROP TABLE IF EXISTS `plants`;
 CREATE TABLE `plants` (
   `plant_id` int(11) NOT NULL,
   `plant_name` varchar(255) NOT NULL,
@@ -68,7 +64,7 @@ CREATE TABLE `plants` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `plants`
+-- Dumping data for table `plants`
 --
 
 INSERT INTO `plants` (`plant_id`, `plant_name`, `plant_category`, `plant_photo`, `plant_description`, `plant_luminosity_min`, `plant_luminosity_max`, `plant_humidity_min`, `plant_humidity_max`, `plant_temperature_min`, `plant_temperature_max`) VALUES
@@ -81,10 +77,9 @@ INSERT INTO `plants` (`plant_id`, `plant_name`, `plant_category`, `plant_photo`,
 -- --------------------------------------------------------
 
 --
--- Structure de la table `uplants`
+-- Table structure for table `uplants`
 --
 
-DROP TABLE IF EXISTS `uplants`;
 CREATE TABLE `uplants` (
   `uplant_id` int(11) NOT NULL,
   `uplant_name` varchar(255) NOT NULL,
@@ -93,26 +88,19 @@ CREATE TABLE `uplants` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `uplants`
+-- Dumping data for table `uplants`
 --
 
 INSERT INTO `uplants` (`uplant_id`, `uplant_name`, `user_id`, `plant_id`) VALUES
 (1, 'guy', 1, 1),
-(3, 'jerry', 1, 2),
-(8, 'noah', 2, 1),
-(10, 'stich', 1, 1),
-(11, 'freddy', 2, 4),
-(13, 'mercury', 2, 3),
-(20, 'cheguevara', 1, 5),
-(21, 'salameche', 2, 3);
+(3, 'jerry', 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `user_username` varchar(255) NOT NULL,
@@ -120,7 +108,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_username`, `user_password`) VALUES
@@ -128,24 +116,24 @@ INSERT INTO `users` (`user_id`, `user_username`, `user_password`) VALUES
 (2, 'antoine', '0e5091a25295e44fea9957638527301f');
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `datas`
+-- Indexes for table `datas`
 --
 ALTER TABLE `datas`
   ADD PRIMARY KEY (`data_id`),
   ADD KEY `FK_uplant_id` (`uplant_id`);
 
 --
--- Index pour la table `plants`
+-- Indexes for table `plants`
 --
 ALTER TABLE `plants`
   ADD PRIMARY KEY (`plant_id`);
 
 --
--- Index pour la table `uplants`
+-- Indexes for table `uplants`
 --
 ALTER TABLE `uplants`
   ADD PRIMARY KEY (`uplant_id`),
@@ -153,47 +141,47 @@ ALTER TABLE `uplants`
   ADD KEY `FK_plant_id` (`plant_id`);
 
 --
--- Index pour la table `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `datas`
+-- AUTO_INCREMENT for table `datas`
 --
 ALTER TABLE `datas`
   MODIFY `data_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT pour la table `plants`
+-- AUTO_INCREMENT for table `plants`
 --
 ALTER TABLE `plants`
-  MODIFY `plant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `plant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
--- AUTO_INCREMENT pour la table `uplants`
+-- AUTO_INCREMENT for table `uplants`
 --
 ALTER TABLE `uplants`
-  MODIFY `uplant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `uplant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `datas`
+-- Constraints for table `datas`
 --
 ALTER TABLE `datas`
   ADD CONSTRAINT `FK_uplant_id` FOREIGN KEY (`uplant_id`) REFERENCES `uplants` (`uplant_id`);
 
 --
--- Contraintes pour la table `uplants`
+-- Constraints for table `uplants`
 --
 ALTER TABLE `uplants`
   ADD CONSTRAINT `FK_plant_id` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`plant_id`),
